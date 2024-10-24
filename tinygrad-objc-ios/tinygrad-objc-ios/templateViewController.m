1902844dfc3339af3ea5470ecc375173a3431fad
Rory Clear <roryclear.rc@gmail.com>
2024-10-24 11:01:22 +0100

    update template

--- tinygrad-objc-ios/tinygrad-objc-ios/templateViewController.m
+++ tinygrad-objc-ios/tinygrad-objc-ios/templateViewController.m
@@ -15,7 +15,7 @@
 
     //CODE HERE
     //END
-    //NSLog(@"%d", *(int32_t *)[x11bfeefc0_9 contents]);
+    //NSLog(@"%d", *((int32_t *)variable));
     NSLog(@"Time: %f seconds", CACurrentMediaTime() - startTime);
 }
 

61d5a79f689db8a2e9784650fa8fd83b720d6854
roryclear <roryclear.rc@gmail.com>
2025-10-27 14:19:00 +0000

    no float4

--- tinygrad/renderer/__init__.py
+++ tinygrad/renderer/__init__.py
@@ -113,7 +113,7 @@ class Renderer:
   device: str = ""
   suffix: str = ""
   # TODO: make this generic with a list of supported types
-  supports_float4: bool = True
+  supports_float4: bool = False
   has_local: bool = True
   has_threads: bool = False
   has_shared: bool = True
